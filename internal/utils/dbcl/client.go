90d499bd944ca615aa4a0bd50278f6ceece378ce
zachmann <gabriel.zachmann@kit.edu>
2021-10-18 11:54:54 +0200

    ignore deepsource warning

--- internal/utils/dbcl/client.go
+++ internal/utils/dbcl/client.go
@@ -11,9 +11,11 @@ import (
 )
 
 // RunDBCommands executes SQL stmts through the mysql cli
-func RunDBCommands(cmds string, dbConfig config.DBConf, printOutput bool) error {
-	mysqlCmd := fmt.Sprintf("mysql -u%s -p%s --protocol tcp -h %s",
-		dbConfig.User, dbConfig.GetPassword(), dbConfig.Hosts[0])
+func RunDBCommands(cmds string, dbConfig config.DBConf, printOutput bool) error { // skipcq RVV-A0005
+	mysqlCmd := fmt.Sprintf(
+		"mysql -u%s -p%s --protocol tcp -h %s",
+		dbConfig.User, dbConfig.GetPassword(), dbConfig.Hosts[0],
+	)
 	if dbConfig.DB != "" {
 		mysqlCmd += fmt.Sprintf(" %s", dbConfig.DB)
 	}
