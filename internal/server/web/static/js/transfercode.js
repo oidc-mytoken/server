d52e3178f7facbc80eef02b010fcf4dd17f141d0
zachmann <gabriel.zachmann@kit.edu>
2022-08-16 14:25:03 +0200

    add transfercode exchange tab

--- internal/server/web/static/js/transfercode.js
+++ internal/server/web/static/js/transfercode.js
@@ -18,3 +18,26 @@ function createTransferCode(token, okCallback, errCallback, endpoint = "") {
         contentType: "application/json"
     });
 }
+
+function exchangeTransferCode() {
+    let tc = $('#tc-input').val();
+    $.ajax({
+        type: "POST",
+        url: storageGet("mytoken_endpoint"),
+        data: JSON.stringify({
+            "grant_type": "transfer_code",
+            "transfer_code": tc
+        }),
+        success: function (data) {
+            let token = data['mytoken'];
+            storageSet("tokeninfo_token", token, true);
+            $('#info-tab').click();
+        },
+        error: function (errRes) {
+            $('#tc-error-modal-msg').text(getErrorMessage(errRes));
+            $('#tc-error-modal').modal();
+        },
+        dataType: "json",
+        contentType: "application/json"
+    });
+}
