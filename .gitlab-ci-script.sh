4b6ba97c7831c0c8b3f7e8926539b26b2d5bc14d
zachmann <gabriel.zachmann@kit.edu>
2022-10-27 08:28:29 +0200

    add more ci actions

--- .gitlab-ci-script.sh
+++ .gitlab-ci-script.sh
@@ -1,11 +1,9 @@
-set -x
 mkdir ../shared
 first=$(grep '^## ' -nm1 CHANGELOG.md | cut -d':' -f1); \
   second=$(grep '^## ' -nm2 CHANGELOG.md | tail -n1 | cut -d':' -f1); \
   tail -n+$first CHANGELOG.md | head -n$(($second-$first)) > ../shared/release.md
-export GORELEASER_CONFIG="$(if echo $CI_COMMIT_TAG | grep -q '-'; then echo '.goreleaser.yml'; else echo '.goreleaser-release.yml'; fi)"
+GORELEASER_CONFIG="$(if echo $CI_COMMIT_TAG | grep -q '-'; then echo '.goreleaser.yml'; else echo '.goreleaser-release.yml'; fi)"
 BASEDIR=/go/src/github.com/oidc-mytoken/server
-ls -l "${PWD}/../shared"
 docker run --rm --privileged \
   -v $PWD:$BASEDIR \
   -w  $BASEDIR\
@@ -15,3 +13,4 @@ docker run --rm --privileged \
   -e GITHUB_TOKEN \
   -e GORELEASER_CONFIG \
   goreleaser/goreleaser release -f $GORELEASER_CONFIG --release-notes /tmp/shared/release.md
+ls -l results
