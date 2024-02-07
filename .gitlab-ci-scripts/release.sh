520606d28346d478c5325f9964aa56a81c792aab
zachmann <gabriel.zachmann@kit.edu>
2024-02-07 15:03:21 +0100

    [ci] debug ci

---
+++ .gitlab-ci-scripts/release.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+set -e
+set -x
+if [ -z "$CI_COMMIT_TAG" ]; then .gitlab-ci-scripts/set-prerel-version.sh; fi;
+.gitlab-ci-scripts/goreleaser.sh
+.gitlab-ci-scripts/upload.sh
+
+curl -d "repo=github.com/oidc-mytoken/server" https://goreportcard.com/checks
