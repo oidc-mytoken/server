71979c8edb8e669d38af7807a557831107cf4ddd
zachmann <gabriel.zachmann@kit.edu>
2024-02-07 15:58:07 +0100

    [ci] debug ci

--- .gitlab-ci-scripts/release.sh
+++ .gitlab-ci-scripts/release.sh
@@ -3,6 +3,7 @@
 set -e
 set -x
 if [ -z "$CI_COMMIT_TAG" ]; then .gitlab-ci-scripts/set-prerel-version.sh; fi;
+docker login -u $DOCKER_USER -p $DOCKER_PASSWORD
 .gitlab-ci-scripts/goreleaser.sh
 .gitlab-ci-scripts/upload.sh
 
